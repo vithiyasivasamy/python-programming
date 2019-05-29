5fade654d00e04b5cc1eeb325a7c9b61366f4f89
vithiyasivasamy <51082203+vithiyasivasamy@users.noreply.github.com>
2019-05-29 07:35:52 -0700

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,5 +1,5 @@
-sx=[a,e,i,o,u]
-sy=[b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z]
+sx=['a','e','i','o','u']
+sy=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 s=input()
 if s in sx:
   print("Vowel")
