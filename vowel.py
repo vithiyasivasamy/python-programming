f3b9bd97a9fa488914871aca9f4c2cec6cc7cf69
vithiyasivasamy <51082203+vithiyasivasamy@users.noreply.github.com>
2019-05-29 07:37:29 -0700

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -3,7 +3,7 @@ sy=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x',
 s=input()
 if s in sx:
   print("Vowel")
-if s in sy:
+elif s in sy:
   print("Consonant")
 else:
   print("Invalid")
