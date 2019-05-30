888e1123edc6defaf3414f0a77bc2946508674f9
vithiyasivasamy <51082203+vithiyasivasamy@users.noreply.github.com>
2019-05-30 09:12:46 -0700

    Update prange.py

--- prange.py
+++ prange.py
@@ -1,11 +1,7 @@
-n,k=list(map(int,input().split()))
-for i in range(n,k):
-    c=0
-    for j in range(1,1000):
+c,w=map(int,input().split())
+for i in range(c+1,w):
+    for j in range(2,i):
         if(i%j==0):
-            c+=1 
-        else:
-            continue
-    if c==2:
+            break
+    else:
         print(i,end=" ")
-
