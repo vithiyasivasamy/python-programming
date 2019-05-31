4e512e3557bfa703795321601e41adccf19068d6
vithiyasivasamy <51082203+vithiyasivasamy@users.noreply.github.com>
2019-05-30 18:26:17 -0700

    Update multiple.py

--- multiple.py
+++ multiple.py
@@ -1,8 +1,7 @@
 ni=int(input())
 fa=1
-if(ni>0):
-    for i in range(1,6):
-        fa=1
-        fa=ni*i
-        print(fa,end=" ")
+for i in range(1,6):
+    fa=1
+    fa=ni*i
+    print(fa,end=" ")
     
