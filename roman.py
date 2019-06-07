aefb13691ebd4f6a0bfdfa594c5e40a0900fd9c0
vithiyasivasamy <51082203+vithiyasivasamy@users.noreply.github.com>
2019-06-07 03:23:35 -0700

    Update roman.py

--- roman.py
+++ roman.py
@@ -1,4 +1,4 @@
 inp =input()
 ijk=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
-if i in List:
+if inp in ijk:
   print(ijk.index(inp))
